320347cbdefbd411357d6d30b240103b52b78159
rohitpal7171 <rohit.pal7171@gmail.com>
2020-10-10 00:10:49 +0530

    second phase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          trying to deploy
+          trying to deploy change in second phase
         </p>
        
       </header>
